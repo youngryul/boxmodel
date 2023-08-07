03e23059783979b7ab9c6372f4e522e6e6241a1c
PNS212624\Hansol <yrlee00@hansol.com>
2023-08-07 17:39:14 +0900

    2023.08.07 box sizing 실습

---
+++ src/main/java/com/example/boxmodel/BoxmodelApplication.java
@@ -0,0 +1,13 @@
+package com.example.boxmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxmodelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxmodelApplication.class, args);
+    }
+
+}
